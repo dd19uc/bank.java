d1921f5e77c42fa4e955d8ed147dc0c8d72d17e3
haider <deluc5000@gmail.com>
2025-10-01 15:06:40 +0300

    add Acc and Acc2 and readme.md files

---
+++ Acc2.java
@@ -0,0 +1,19 @@
+package packag;
+
+public class Acc2 {
+    public static void main(String args []){
+       
+  Acc a =new Acc();
+    a.insert(4, "Jhon" ,5400000.0f);
+    a.deposit(1000000.0f);
+    a.withdraw(1500000.0f);
+    a.checkBalance();
+        System.out.println("the account details : " +a.toString());
+  Acc a2 = new Acc();
+    a2.insert(5, "diana", 1000000.0f);
+    a2.deposit(500000.0f);      
+    a2.withdraw(600000.0f);
+    a2.checkBalance();
+        System.out.println("the account details : "+a2.toString());
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Hello, this is just simple code about a bank statments like withdraw and somethings eles. 
+## its not that pro code but im still learnning tho 😅 .
